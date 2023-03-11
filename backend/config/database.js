a3565d0a5dfb9dbeddd1bb70ba88b16931f580ea
Omkar Borker <omkarborker15@gmail.com>
2023-03-11 17:03:07 +0530

    modified:   backend/config/database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,6 @@
 import mongoose from 'mongoose'
 
-// Local database connection
-// mongoose.connect('mongodb://localhost:27017/convin', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }, () => {
-//     console.log("DB Connected")
-// });
-
-// Mongodb connection
-mongoose.connect(`mongodb+srv://${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PASSWORD}@cluster0.nalllch.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.nalllch.mongodb.net/?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => {
